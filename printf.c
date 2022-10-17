ff1af42fdb78d03eb681dde04fb09df6e4743b23
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-10-17 05:44:05 -0700

    Write a function that produces output according to a format.
    Prototype: int _printf(const char *format, ...);
    Returns: the number of characters printed (excluding the null byte used to end output to strings)
    write output to stdout, the standard output stream
    format is a character string. The format string is composed of zero or more directives. See man 3 printf for more detail. You need to handle the following conversion specifiers:
    c
    s
    %
    You don’t have to reproduce the buffer handling of the C library printf function
    You don’t have to handle the flag characters
    You don’t have to handle field width
    You don’t have to handle precision
    You don’t have to handle the length modifiers

--- printf.c
+++ printf.c
@@ -8,10 +8,39 @@
 
 int _printf(const char *format, ...)
 {
+	unsigned int i, x, len;
+	int tot_len;
+	char *str;
 	va_list list;
 
+	s_f sf[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_perc},
+		{NULL, NULL}
+	};
 	va_start(list, format);
-	(*get_snf_func(format))(list);
-	va_end(list);
-	return (0);
+	if (format != NULL)
+	{
+		len = strlen(format);
+		str = malloc(sizeof(char) * (len + 1));
+		str = strcpy(str, format);
+		i = 0;
+		while (i < len)
+		{
+			x = 0;
+			while (sf[x].symbol != NULL)
+			{
+				if (*(sf[x].symbol) == *(str + i))
+				{
+					tot_len = (*(sf[x].function))(list);
+				}
+				x++;
+			}
+			i++;
+		}
+		va_end(list);
+		free(str);
+	}
+	return (tot_len);
 }
